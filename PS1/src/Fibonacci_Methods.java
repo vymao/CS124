77397b257f7a20ac3216137a27bd9195ebf04100
Victor <victor.mao@berkeley.edu>
2020-02-05 21:57:51 -0500

    Corrected modular Matrix exponentiation

--- PS1/src/Fibonacci_Methods.java
+++ PS1/src/Fibonacci_Methods.java
@@ -14,31 +14,20 @@ public class Fibonacci_Methods {
     }
 
     public static int Fib_iter(long n) {
-        /*
-        int[] sequence = new int[n + 1];
-        sequence[0] = 0;
-        sequence[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            sequence[i] = (sequence[i - 1] + sequence[i - 2]) % correction;
-        }
-        */
-        int previouspreviousNumber, previousNumber = 0, currentNumber = 1;
+        int ppNum, pNum = 0, current = 1;
 
         for (int i = 1; i < n ; i++) {
+            ppNum = pNum;
+            pNum = current;
 
-            previouspreviousNumber = previousNumber;
-
-            previousNumber = currentNumber;
-
-            currentNumber = (previouspreviousNumber + previousNumber) % correction;
-
+            current = (ppNum + pNum) % correction;
         }
-        return currentNumber;
+        return current;
         ///return sequence[n];
     }
 
-    public static int[][] Matrix_Mult(int[][] m1, int[][] m2) {
-        int[][] matrix1, matrix2;
+    public static long[][] Matrix_Mult(long[][] m1, long[][] m2) {
+        long[][] matrix1, matrix2;
 
         if (!((m1[0].length == m2.length) || m2[0].length == m1.length)) {
             return null;
@@ -50,10 +39,10 @@ public class Fibonacci_Methods {
             matrix2 = m1;
         }
 
-        int[][] product = new int[matrix1.length][matrix2[0].length];
+        long[][] product = new long[matrix1.length][matrix2[0].length];
         for (int i = 0; i < matrix1.length; i++) {
             for (int j = 0; j < matrix2[0].length; j++) {
-                int sum = 0;
+                long sum = 0;
 
                 for (int k = 0; k < matrix1[0].length; k++) {
                     sum = (sum + ((matrix1[i][k] * matrix2[k][j]) % correction)) % correction;
@@ -66,12 +55,12 @@ public class Fibonacci_Methods {
         return product;
     }
 
-    public static int Fib_matrix(int n) {
+    public static int Fib_matrix(long n) {
         if (n == 0) {
             return 0;
         } else {
-            int[][] FibMatrix = {{0, 1}, {1, 1}};
-            int[][] product = {{1, 0}, {0, 1}};
+            long[][] FibMatrix = {{0, 1}, {1, 1}};
+            long[][] product = {{1, 0}, {0, 1}};
 
             while (n > 0) {
                 if (n % 2 == 1) {
@@ -84,12 +73,61 @@ public class Fibonacci_Methods {
 
             }
 
-            int[][] Fib_start = {{0}, {1}};
-            int[][] Fib = Matrix_Mult(product, Fib_start);
+            long[][] Fib_start = {{0}, {1}};
+            long[][] Fib = Matrix_Mult(product, Fib_start);
 
-            return Fib[0][0];
+            return (int) Fib[0][0];
         }
     }
+
+    public static int[] run_test(double time) {
+        long startTime, endTime;
+        int n = 1;
+        int[] times = new int[3];
+
+        while (n > 0) {
+            startTime = System.nanoTime();
+            int iter_sol = Fib_recursive(n);
+            endTime = System.nanoTime();
+            double timed = (double) (endTime - startTime) / ((double) 1000000000);
+            if (timed > time) {
+                times[0] = n - 1;
+                System.out.println("Recursive maximum in 10 seconds: " + Integer.toString(n - 1));
+                break;
+            }
+            n++;
+        }
+
+        while (n > 0) {
+            startTime = System.nanoTime();
+            int iter_sol = Fib_iter(n);
+            endTime = System.nanoTime();
+            double timed = (double) (endTime - startTime) / ((double) 1000000000);
+            if (timed > time) {
+                times[1] = n - 1;
+                System.out.println("Iterative maximum in 10 seconds: " + Integer.toString(n - 1));
+                break;
+            }
+            n++;
+        }
+
+        while (n > 0) {
+            startTime = System.nanoTime();
+            int iter_sol = Fib_matrix(n);
+            endTime = System.nanoTime();
+            double timed = (double) (endTime - startTime) / ((double) 1000000000);
+            if (timed > time) {
+                times[2] = n - 1;
+                System.out.println("Matrix maximum in 10 seconds: " + Integer.toString(n - 1));
+                break;
+            }
+            n++;
+        }
+
+        return times;
+
+    }
+
 
     public static void main(String[] args) {
         long startTime, endTime;
@@ -100,22 +138,22 @@ public class Fibonacci_Methods {
         double Fib_recursive_time = ((double) (endTime - startTime) / ((double) 1000000000));
         System.out.println("Recursive solution: " + Integer.toString(recursive_solution));
         System.out.println("Recursive solution time: " + Double.toString(Fib_recursive_time) + " seconds");
-        */
+
         startTime = System.nanoTime();
         int iter_solution = Fib_iter(Long.parseLong(args[0]));
         endTime = System.nanoTime();
         double Fib_iter_time = (double) ((double) (endTime - startTime) / ((double) 1000000000));
         System.out.println("Iterative solution: " + Integer.toString(iter_solution));
         System.out.println("Iterative solution time: " + Double.toString(Fib_iter_time) + " seconds");
-
+*/
         startTime = System.nanoTime();
-        int matrix_solution = Fib_matrix(Integer.parseInt(args[0]));
+        int matrix_solution = Fib_matrix(Long.parseLong(args[0]));
         endTime = System.nanoTime();
         double Fib_matrix_time = (double) ((double) (endTime - startTime) / ((double) 1000000000));
         System.out.println("Matrix solution: " + Integer.toString(matrix_solution));
         System.out.println("Matrix solution time: " + Double.toString(Fib_matrix_time) + " seconds");
 
-
+        //int[] times = run_test(10);
     }
 
 }
